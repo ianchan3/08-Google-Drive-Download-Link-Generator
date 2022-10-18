b6ce71106aff0a025b47d49d55e781e9a33433fb
ianchan3 <ianchan3138@gmail.com>
2022-10-18 15:55:20 -0700

    Partial JavaScript Logic Completed

---
+++ js/app.js
@@ -0,0 +1,33 @@
+const gLink = document.getElementById("glink");
+const btn = document.getElementById("btn");
+const downloadLink = document.getElementById("download-link");
+
+btn.addEventListener("click", generateLink)
+
+function generateLink(e) {
+  e.preventDefault();
+  const gLinkValue = gLink.value;
+  const confirmLink = gLink.value.includes("https://drive.google.com/file/d/");
+
+  if (confirmLink == true) {
+    const getDownloadLink = gLink.value
+    .replace("https://drive.google.com/file/d/", "https://drive.google.com/uc?export=download&id=")
+    .replace("/view?usp=sharing", "");
+
+    downloadLink.value = getDownloadLink;
+
+    function copyText(target) {
+      if (target.value == "") {
+        alert("Please generate a download link")
+      } else {
+        target.select();
+        document.execCommand("copy");
+        alert("Link has been copied to clipboard");
+      }
+      }
+      const copy = document.querySelector(".copy")
+      copy.addEventListener("click", () => {
+        return copyText(downloadLink)
+      }
+    }
+  }
